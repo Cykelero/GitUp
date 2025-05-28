f1f18c4d9de4896803ee6c01c1e221758e7432ba
Nathan Manceaux-Panot <nathan@lemon.garden>
2025-05-28 15:56:11 +0200

    Update stat cache when writing files

--- GitUpKit/Core/GCIndex.m
+++ GitUpKit/Core/GCIndex.m
@@ -664,7 +664,7 @@ cleanup:
 	}
 	
   git_checkout_options options = GIT_CHECKOUT_OPTIONS_INIT;
-  options.checkout_strategy = GIT_CHECKOUT_FORCE | GIT_CHECKOUT_DONT_UPDATE_INDEX;  // There's no reason to update the index
+  options.checkout_strategy = GIT_CHECKOUT_FORCE;
   options.paths.count = paths.count;
   char** pathStrings = malloc(paths.count * sizeof(char*));
   options.paths.strings = pathStrings;
